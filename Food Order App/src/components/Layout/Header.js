b8ad0fa3786669656fe9faa60850ff87c9bad09b
Divyanshu <agarwaldivyanshu69@gmail.com>
2022-10-04 15:47:51 +0530

    HeaderButton modification

--- Food Order App/src/components/Layout/Header.js
+++ Food Order App/src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealImage from "../../assest/meals.jpg";
+import HeaderButton from "./HeaderButton";
 
 const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderButton></HeaderButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealImage} alt="Delicious Food"></img>
